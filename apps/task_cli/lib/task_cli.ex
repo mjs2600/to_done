79894c1db0fbc333312249eb8fdcd794a77be85d
Michael Simpson <mjs2600@gmail.com>
2016-11-25 10:33:44 -0500

    Add basic CLI

--- apps/task_cli/lib/task_cli.ex
+++ apps/task_cli/lib/task_cli.ex
@@ -1,21 +1,44 @@
 defmodule TaskCLI do
-  def main(_args) do
-    TaskKeeper.Task.active
-    |> TaskKeeper.Repo.all
+  alias TaskKeeper.{Repo, Task}
+
+  def main(args) do
+    {switches, _args, _errors} = OptionParser.parse(args)
+    run(switches)
+  end
+
+  defp run(complete: id) do
+    Task
+    |> Repo.get(id)
+    |> Task.changeset(%{completed: true})
+    |> Repo.update
+
+    run(show: id)
+  end
+
+  defp run(show: id) do
+    Task
+    |> Repo.get(id)
+    |> format
+    |> IO.puts
+  end
+
+  defp run(_) do
+    Task.active
+    |> Repo.all
     |> Enum.map(&format/1)
     |> Enum.join("\n")
     |> IO.puts
   end
 
   defp format(task) do
-    "#{task |> format_completed} | #{task.name}"
+    "#{task |> format_completed} | #{task.id} | #{task.name}"
   end
 
-  defp format_completed(%TaskKeeper.Task{completed: true}) do
+  defp format_completed(%Task{completed: true}) do
     "✓"
   end
 
-  defp format_completed(%TaskKeeper.Task{}) do
+  defp format_completed(%Task{}) do
     "✗"
   end
 
